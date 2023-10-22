b826d72cfa3d9ebfb964477aaf9ed22775691b0a
hygsani <blek.wind@gmail.com>
2023-10-22 11:04:25 +0700

    [update] register guestbook route

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'html')
 
 const indexRouter = require('./routes/index')
+const guestbookRouter = require('./routes/guestbook')
 
 app.use('/', indexRouter)
+app.use('/guestbooks', guestbookRouter)
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
---
+++ routes/guestbook.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+
+router.get('/', (req, res, any) => {
+    db.any(`SELECT * FROM guestbooks`)
+        .then((data) => {
+            res.render('guestbooks/index', { guestbooks: data })
+        })
+        .catch((err) => {
+            console.log(`err: ${err}`)
+        })
+})
+
+router.get('/create', (req, res, any) => {
+    res.render('guestbooks/create')
+})
+
+module.exports = router
