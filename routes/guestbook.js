0d02fb425908f35c112bb8e539536d950774694e
hygsani <blek.wind@gmail.com>
2023-10-23 20:52:35 +0700

    [update] implement form validation & insert data

--- routes/guestbook.js
+++ routes/guestbook.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 const db = require('../db')
+
+const formValidator = [
+    body('name', 'name must not empty').not().isEmpty(),
+    body('email', 'invalid email').isEmail()
+]
 
 router.get('/', (req, res, next) => {
     db.any(`SELECT * FROM guestbooks`)
@@ -14,6 +20,23 @@ router.get('/', (req, res, next) => {
 
 router.get('/create', (req, res, next) => {
     res.render('guestbooks/create')
+})
+
+router.post('/store', formValidator, (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+        db.one('INSERT INTO guestbooks(name, email, message) VALUES($1, $2, $3) RETURNING guestbook_id', [req.body.name, req.body.email, req.body.message])
+            .then((data) => {
+                res.redirect('/guestbooks')
+            })
+            .catch((err) => {
+                console.log(`err: ${err}`)
+            })
+    } else {
+        res.render('guestbooks/create', { errors: errors.array() })
+        return
+    }
 })
 
 module.exports = router
